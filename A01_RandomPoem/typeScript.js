90c5e86b23a7cc02c7ae4aaf3698053de79e2459
SilvaHomeNigra <carolin.albus@live.de>
2024-03-30 17:04:23 +0100

    fix

--- A01_RandomPoem/typeScript.js
+++ A01_RandomPoem/typeScript.js
@@ -1,4 +1,10 @@
 "use strict";
+/*
+Aufgabe: L01_Zufallsgedicht
+Name: Carolin Albus
+Datum: 30.04.24
+Quellen: Elisabeth Krum
+*/
 let subjekt = ["Maria", "Thomas", "Laura", "Niklas", "Lotta", "Sarah"];
 let predicate = ["läuft", "spielt", "kocht", "liest", "schläft", "tanzt"];
 let object = ["Spaghetti", "ein Buch", "Zumba", "ganz schnell", "ein Spiel", "tief und fest"];
@@ -13,9 +19,18 @@ for (let i = subjekt.length; i >= 1; i--) {
 }
 function getVerse(subjekt, predicate, object) {
     let vers = "";
-    return "HalliHallo";
+    let randomSubjectIndex = Math.floor(Math.random() * subjekt.length);
+    // console.log(randomSubjectIndex);
+    let randomSubject = subjekt.splice(randomSubjectIndex, 1)[0];
+    vers += randomSubject + " ";
+    let randomPredicateIndex = Math.floor(Math.random() * predicate.length);
+    // console.log(randomPredicateIndex);
+    let randomPredicate = predicate.splice(randomPredicateIndex, 1)[0];
+    vers += randomPredicate + " ";
+    let randomObjectIndex = Math.floor(Math.random() * object.length);
+    // console.log(randomObjectIndex);
+    let randomObject = object.splice(randomObjectIndex, 1)[0];
+    vers += randomObject;
+    return vers;
 }
-// Aufruf der Funktion
-//const generatedVerse = getVerse(subjekt, predicate, object);
-//console.log("Generierter Vers:", generatedVerse);
 //# sourceMappingURL=typeScript.js.map
--- A01_RandomPoem/typeScript.ts
+++ A01_RandomPoem/typeScript.ts
@@ -1,3 +1,11 @@
+/*
+Aufgabe: L01_Zufallsgedicht
+Name: Carolin Albus
+Datum: 30.04.24
+Quellen: Elisabeth Krum
+*/
+
+
 let subjekt:string[]= ["Maria","Thomas","Laura","Niklas","Lotta","Sarah"]
 let predicate:string[]= ["läuft","spielt","kocht","liest","schläft","tanzt"]
 let object:string[]= ["Spaghetti","ein Buch","Zumba","ganz schnell","ein Spiel","tief und fest"]
@@ -15,12 +23,23 @@ for (let i = subjekt.length; i >= 1; i--) {
 
 function getVerse(subjekt:string[], predicate:string[], object:string[]): string {
     let vers: string = "";
+
+    let randomSubjectIndex: number = Math.floor(Math.random() * subjekt.length);
+    // console.log(randomSubjectIndex);
+    let randomSubject: string = subjekt.splice(randomSubjectIndex, 1)[0];
+    vers += randomSubject + " ";
     
+    let randomPredicateIndex: number = Math.floor(Math.random() * predicate.length);
+    // console.log(randomPredicateIndex);
+    let randomPredicate: string = predicate.splice(randomPredicateIndex, 1)[0];
+    vers += randomPredicate + " ";
     
-    return "HalliHallo"; 
+    let randomObjectIndex: number = Math.floor(Math.random() * object.length);
+    // console.log(randomObjectIndex);
+    let randomObject: string = object.splice(randomObjectIndex, 1)[0];
+    vers += randomObject;
+    
+    return vers;
 }
 
-// Aufruf der Funktion
-//const generatedVerse = getVerse(subjekt, predicate, object);
-//console.log("Generierter Vers:", generatedVerse);
  
